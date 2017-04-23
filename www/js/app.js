a530fe3d69c58a7f17df709329214575f6b2c494
Flavien94 <favien_94@hotmail.fr>
2017-04-23 19:26:01 +0200

    commit for deploy to heroku

--- www/js/app.js
+++ www/js/app.js
@@ -36,4 +36,6 @@ app.config(function($stateProvider, $urlRouterProvider) {
 
   $urlRouterProvider.otherwise('/');
 });
-app.listen(process.env.PORT || 5000)
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
