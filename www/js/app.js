4a8289baf168f241b7b285684ed67b09950d7346
Flavien94 <favien_94@hotmail.fr>
2017-04-23 19:35:48 +0200

    commit for deploy to heroku

--- www/js/app.js
+++ www/js/app.js
@@ -1,5 +1,5 @@
 var app = angular.module('starter', ['ionic', 'ui.router']);
-app.listen(process.env.PORT || 3000, function(){
+app.listen(process.env.PORT || 8000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
 app.run(function($ionicPlatform) {
